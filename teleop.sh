1cebeda1081b4829414b76eb23c8fd0ac02b7dc6
Arika <arikak@ou.edu>
2025-10-24 10:10:33 +0000

    teleop now correct (but need to run in vnav still). This should be merged later
    added SLAM config file :fire:
    
    up /idea into git ignore

--- teleop.sh
+++ teleop.sh
@@ -1,1 +1,6 @@
-ros2 launch launches teleop.launch.py
+cd ~/Vnavros2setup/workspaces/f1tenth_ws/src
+source install/setup.sh
+ros2 launch launches teleop.launch.py joy_dev:=ttyUSB0
+
+
+#TODO merge into main repo later!
